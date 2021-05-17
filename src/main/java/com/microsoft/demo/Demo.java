f4e54e30f6eef4e663b0ed37c3a9973b8a741bcb
slahirucd7 <slahiruc007@gmail.com>
2021-05-17 09:29:18 +0530

    new branch creation and file edit

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -1,6 +1,11 @@
 package com.microsoft.demo;
 
 public class Demo {
+
+    public static void main(String arg[]){
+        System.out.println("This runs in the test branch");
+    }
+    
     public void DoSomething(boolean flag){
         if(flag){
             System.out.println("I am covered");
